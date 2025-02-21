7e693dd9369be6d3a7938d8947fd4a1bb5a1da62
Paul Vallas <vallasff@hotmail.com>
2025-02-21 12:47:27 +0200

    When user submits a city, the weather is logged

--- js/index.js
+++ js/index.js
@@ -2,5 +2,19 @@ import '../css/reset.css';
 import '../css/style.css';
 
 import weather from './weather.js';
+import constraintValidation from './constraintValidation.js';
 
-weather('london');
+// Get a reference to the form searchbox
+const searchbox = document.querySelector('#searchbox');
+
+// Get a reference to the form
+const myForm = document.querySelector('form');
+
+// Constraint validation to show a custom message
+constraintValidation(searchbox);
+
+// Pass the searchbox value to get the location weather
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    weather(searchbox.value);
+});
