69dfd3f720d6b344ffb758bacc306a2acce64fe1
Paul Vallas <vallasff@hotmail.com>
2025-02-23 20:32:51 +0200

    Repurposed module

--- js/displayWeatherIcon.js
+++ js/displayWeatherIcon.js
@@ -1,18 +1,18 @@
-function displayWeatherIcon(weatherCode) {
-    const weatherImage = document.querySelector('.today_card img');
+function displayWeatherIcon(weatherImage, weatherCode) {
+    const weatherIcon = weatherImage;
 
     switch (weatherCode) {
         case 0:
-            weatherImage.src = 'images/weather-icons/clear.svg';
+            weatherIcon.src = 'images/weather-icons/clear.svg';
             break;
         case 1:
         case 2:
         case 3:
-            weatherImage.src = 'images/weather-icons/partly-clear.svg';
+            weatherIcon.src = 'images/weather-icons/partly-clear.svg';
             break;
         case 45:
         case 48:
-            weatherImage.src = 'images/weather-icons/cloudy.svg';
+            weatherIcon.src = 'images/weather-icons/cloudy.svg';
             break;
         case 51:
         case 53:
@@ -22,14 +22,14 @@ function displayWeatherIcon(weatherCode) {
         case 80:
         case 81:
         case 82:
-            weatherImage.src = 'images/weather-icons/rainy.svg';
+            weatherIcon.src = 'images/weather-icons/rainy.svg';
             break;
         case 61:
         case 63:
         case 65:
         case 66:
         case 67:
-            weatherImage.src = 'images/weather-icons/partly-rainy.svg';
+            weatherIcon.src = 'images/weather-icons/partly-rainy.svg';
             break;
         case 71:
         case 73:
@@ -37,12 +37,12 @@ function displayWeatherIcon(weatherCode) {
         case 77:
         case 85:
         case 86:
-            weatherImage.src = 'images/weather-icons/partly-snowy.svg';
+            weatherIcon.src = 'images/weather-icons/partly-snowy.svg';
             break;
         case 95:
         case 96:
         case 99:
-            weatherImage.src = 'images/weather-icons/thunder.svg';
+            weatherIcon.src = 'images/weather-icons/thunder.svg';
             break;
         default:
             break;
