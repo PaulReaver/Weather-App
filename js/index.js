b2a3c96d63358c4f024d612eb7ffb0c0f477e368
Paul Vallas <vallasff@hotmail.com>
2025-02-24 12:32:10 +0200

    Moved reusable code to a module

--- js/index.js
+++ js/index.js
@@ -3,32 +3,17 @@ import '../css/style.css';
 
 import weather from './weather.js';
 import constraintValidation from './constraintValidation.js';
-import displayLocation from './displayLocation.js';
-import displayWeatherIcon from './displayWeatherIcon.js';
-import assignDates from './assignDates.js';
+import displayAll from './displayAll.js';
 
-// Initial city given and temperature display
-async function initializeWeather() {
+// Initial city given and temperature display with IIFE
+(async function initializeWeather() {
     try {
         const weatherData = await weather('London');
-
-        displayLocation(weatherData.location);
-        displayWeatherIcon(
-            document.querySelector('.today_card img'),
-            weatherData.currentWeather.weatherCode,
-            weatherData.currentWeather.isDay
-        );
-        document.querySelector('.current_temperature_wrapper h2').textContent =
-            `${Math.round(weatherData.currentWeather.temperature_2m)}°`;
-        document.querySelector('.current_temperature_wrapper p').textContent =
-            `Feels like ${Math.round(weatherData.currentWeather.apparentTemperature)}°`;
-        assignDates();
+        displayAll(weatherData);
     } catch (error) {
         throw new Error('Error initializing weather');
     }
-}
-
-initializeWeather(); // Call the initialization function
+})();
 
 // Get a reference to the form searchbox and form
 const searchbox = document.getElementById('searchbox');
@@ -43,17 +28,7 @@ myForm.addEventListener('submit', async (e) => {
 
     try {
         const weatherData = await weather(searchbox.value);
-        displayLocation(weatherData.location);
-        displayWeatherIcon(
-            document.querySelector('.today_card img'),
-            weatherData.currentWeather.weatherCode,
-            weatherData.currentWeather.isDay
-        );
-        document.querySelector('.current_temperature_wrapper h2').textContent =
-            `${Math.round(weatherData.currentWeather.temperature_2m)}°`;
-        document.querySelector('.current_temperature_wrapper p').textContent =
-            `Feels like ${Math.round(weatherData.currentWeather.apparentTemperature)}°`;
-        assignDates();
+        displayAll(weatherData);
     } catch (error) {
         throw new Error('Error fetching weather');
     }
